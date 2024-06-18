cf62e0fa1e531f6de539b168db7f5d65c8fc73d8
Hainer Stefan <hainer.stefan@gmail.com>
2024-06-18 11:30:52 +0300

    added pagination properties...

--- src/components/Paginations.js
+++ src/components/Paginations.js
@@ -1,37 +1,115 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import paginationArrow from "../assets/pagination-arrow.svg";
+import { set } from "lodash";
+import { CryptoContext } from "../context/CryptoContext";
 
 const Paginations = () => {
+  let { page, setPage, totalPages } = useContext(CryptoContext);
+  const TotalNumber = Math.ceil(totalPages / 10);
+
+  const next = () => {
+    if (page === TotalNumber) {
+      return null;
+    } else {
+      setPage(page + 1);
+    }
+  };
+
+  const prev = () => {
+    if (page === 1) {
+      return null;
+    } else {
+      setPage(page - 1);
+    }
+  };
+
+  const multiStepNext = () => {
+    if (page + 3 >= TotalNumber) {
+      setPage(TotalNumber - 1);
+    } else {
+      setPage(page + 3);
+    }
+  };
+
+  const multiStepPrev = () => {
+    if (page - 3 <= 1) {
+      setPage(TotalNumber + 1);
+    } else {
+      setPage(page - 2);
+    }
+  };
+
   return (
     <div className="flex items-center">
       <ul className="flex items-center justify-end text-sm">
         <li className="flex items-center">
-          <button className="outline-0 hover:text-cyan w-8">
+          <button className="outline-0 hover:text-cyan w-8" onClick={prev}>
             <img className="w-full h-auto rotate-180" src={paginationArrow} alt="left arrow" />
           </button>
         </li>
+        {page + 1 === TotalNumber || page === TotalNumber ? (
+          <li>
+            <button
+              onClick={multiStepPrev}
+              className="outline-0 hover:text-cyan rounded-full w-8 h-8 flex items-center justify-center text-lg"
+            >
+              ...
+            </button>
+          </li>
+        ) : null}
+        {page - 1 !== 0 ? (
+          <li>
+            <button
+              onClick={prev}
+              className="outline-0 hover:text-cyan rounded-full w-8 h-8 flex items-center justify-center bg-gray-200"
+            >
+              {page - 1}
+            </button>
+          </li>
+        ) : null}
         <li>
-          <button>...</button>
+          <button
+            disabled
+            className="outline-0  rounded-full w-8 h-8 flex items-center justify-center bg-cyan text-gray-300 mx-1.5"
+          >
+            {page}
+          </button>
         </li>
-        <li>
-          <button>1</button>
-        </li>
-        <li>
-          <button>2</button>
-        </li>
-        <li>
-          <button>3</button>>
-        </li>
-        <li>
-          <button>...</button>
-        </li>
-        <li>
-          <button>last-page</button>
-        </li>
-        <li>
-          <button>
+        {page + 1 !== TotalNumber && page !== TotalNumber ? (
+          <li onClick={next}>
+            <button className="outline-0 hover:text-cyan rounded-full w-8 h-8 flex items-center justify-center bg-gray-200 mx-1.5">
+              {page + 1}
+            </button>
+          </li>
+        ) : null}
+        {page + 1 !== TotalNumber && page !== TotalNumber ? (
+          <li>
             {" "}
-            <img className="w-full h-auto" src={paginationArrow} alt="left arrow" />
+            <button
+              onClick={multiStepNext}
+              className="outline-0 hover:text-cyan rounded-full w-8 h-8 flex items-center justify-center text-lg"
+            >
+              ...
+            </button>
+          </li>
+        ) : null}
+
+        {page !== TotalNumber ? (
+          <li>
+            <button
+              onClick={() => {
+                setPage(TotalNumber);
+              }}
+              className="outline-0 hover:text-cyan rounded-full w-8 h-8 flex items-center justify-center bg-gray-200 mx-1.5"
+            >
+              {TotalNumber}
+            </button>
+          </li>
+        ) : null}
+        <li>
+          <button className="outline-0 hover:text-cyan w-8" onClick={next}>
+            {" "}
+            <img className="w-full h-auto" src={paginationArrow} alt="right arrow" />
           </button>
         </li>
       </ul>
--- src/context/CryptoContext.js
+++ src/context/CryptoContext.js
@@ -10,6 +10,8 @@ export const CryptoProvider = ({ children }) => {
   const [coinSearch, setCoinSearch] = useState("");
   const [currency, setCurrency] = useState("usd");
   const [sortBy, setSortBy] = useState("market_cap_desc");
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(250);
 
   const getCoinData = async () => {
     try {
@@ -28,8 +30,19 @@ export const CryptoProvider = ({ children }) => {
 
   const getCryptoData = async () => {
     try {
+      const data = await fetch(`https://api.coingecko.com/api/v3/coins/list`)
+        .then((res) => res.json())
+        .then((json) => json);
+
+      // console.log(data);
+      setTotalPages(data.length);
+    } catch (error) {
+      console.log(error);
+    }
+
+    try {
       const data = await fetch(
-        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&ids=${coinSearch}&order=${sortBy}&per_page=10&price_change_percentage=1h%2C24h%2C7d`
+        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&ids=${coinSearch}&order=${sortBy}&per_page=10&page=${page}&price_change_percentage=1h%2C24h%2C7d`
       )
         .then((res) => res.json())
         .then((json) => json);
@@ -54,9 +67,14 @@ export const CryptoProvider = ({ children }) => {
     }
   };
 
+  const resetFunc = () => {
+    setPage(1);
+    setCoinSearch("");
+  };
+
   useLayoutEffect(() => {
     getCryptoData();
-  }, [coinSearch, currency, sortBy]);
+  }, [coinSearch, currency, sortBy, page]);
 
   return (
     <CryptoContext.Provider
@@ -70,9 +88,16 @@ export const CryptoProvider = ({ children }) => {
         setCurrency,
         sortBy,
         setSortBy,
+        page,
+        setPage,
+        totalPages,
+        setTotalPages,
+        resetFunc,
       }}
     >
       {children}
     </CryptoContext.Provider>
   );
 };
+
+///added pagination features + page reset button
